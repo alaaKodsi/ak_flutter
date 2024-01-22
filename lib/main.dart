ae655786f1c08df2db47fc88e6b64fd25cc9d245
alaaKodsi <alaakodsy.1@gmail.com>
2024-01-22 22:06:18 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AkTest(),
+    );
+  }
+}
+
+class AkTest extends StatefulWidget {
+  const AkTest({super.key});
+
+  @override
+  State<AkTest> createState() => _AkTestState();
+}
+
+class _AkTestState extends State<AkTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black87,
+          title: const Text(
+            "AK Flutter",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+        ),
+        body: const Center());
+  }
+}
