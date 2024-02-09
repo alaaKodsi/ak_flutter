60e800ee3e7aaeacd4027078e39c00341694b863
alaaKodsi <alaakodsy.1@gmail.com>
2024-02-09 21:56:39 +0300

    add firebase to Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:aktest/splashScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const AkTest(),
     );
   }
 }
