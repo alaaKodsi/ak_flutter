8788ce282a6fa1ba5135f79e133563ead10d4568
alaaKodsi <alaakodsy.1@gmail.com>
2024-03-05 00:19:59 +0300

    Create Login & Register Screen

---
+++ lib/core/constant/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color primryColor = Color(0xffff9f4b);
+const Color secondColors = Color(0xff232a39);
+const Color backgroundColor = Color(0xff1a1c2b);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:aktest/webView.dart';
+import 'package:aktest/view/screen/loginPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +11,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AkTest(),
+      home: const LoginPage(),
     );
   }
 }
@@ -51,18 +50,9 @@ class _AkTestState extends State<AkTest> {
             children: [
               Image.asset(
                 "assets/image/AK Flutter.png",
-                width: 250,
-                height: 200,
+                width: 300,
+                height: 300,
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const WebView()));
-                  },
-                  child: const Text(
-                    "Open Web Page",
-                    style: TextStyle(color: Colors.black54),
-                  ))
             ],
           ),
         ));
